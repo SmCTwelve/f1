f95d23fcd52d05eba0004ff9b435e68ce7cf4ec5
SmCTwelve <smctwelve@gmail.com>
2017-10-31 17:30:53 +0000

    Fix event handler. Store index in state.

--- src/Team.jsx
+++ src/Team.jsx
@@ -9,13 +9,12 @@ class Team extends Component {
   constructor(props) {
     super(props);
 
-    // `index` -- whether to render index page with team selection (default landing page).
-    this.index = true;
-
     // State:
     this.state = {
-      // the team to display, updates styles and content based on selected team.
-      team: null
+      // `team` -- to display, updates styles and content based on selected team.
+      // `index` -- whether to render index page with team selection (default landing page).
+      team: "",
+      index: true
     };
 
     this.updateTeam = this.updateTeam.bind(this);
@@ -23,14 +22,17 @@ class Team extends Component {
 
   // Change state of component to the chosen team
   updateTeam(team) {
-    this.setState({team: team}, () => {
-      this.index = false;
+    this.setState({
+      team: team,
+      index: false
+    }, () => {
       console.log("SELECTED TEAM: " + team);
+      console.log(this.index);
     });
   }
 
   render() {
-    if (!this.index) {
+    if (!this.state.index) {
       return(
         <div>
           <div className={"hero-container " + this.state.team}>
