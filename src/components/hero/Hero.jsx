2bde1ff1b62b0f81f493ebeedbb76bd8a5ab6e35
SmCTwelve <smctwelve@gmail.com>
2017-10-31 03:19:58 +0000

    Add renault and redbull content

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
+import Logo from '../logo/Logo.jsx';
 import Ferrari from '../../images/car-fer.png';
+import Redbull from '../../images/car-rbr.png';
+import Renault from '../../images/car-rsr.png';
 
 /**
  * Hero image component. Renders the team title and car in the header.
  *
  * Props:
- * `team` -- 3 letter team suffix, e.g. 'rbr', 'fer'.
- * `title` -- full team name to display in header.
+ * `team` -- team name, 'williams', 'redbull'.
+ * `title` -- full team title to display in header, e.g. 'Scuderia Ferrari'.
  * @param {*} props
  */
 const Hero = (props) => {
 
+  // Get correct car image
   let url = "";
   switch (props.team) {
-    case "fer":
+    case "ferrari":
       url = Ferrari;
+      break;
+    case "redbull":
+      url = Redbull;
+      break;
+    case "renault":
+      url = Renault;
       break;
 
     default:
@@ -23,6 +33,7 @@ const Hero = (props) => {
 
   return(
     <div className="hero content">
+      <Logo team={props.team} />
       <img src={url} alt={props.title} />
       <h3 className="hdg">{props.title}</h3>
     </div>
