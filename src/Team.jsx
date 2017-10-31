61f4024ade211f62dcebf84c9d19c6d0700c01bf
SmCTwelve <smctwelve@gmail.com>
2017-10-31 03:25:01 +0000

    Move index out of state. Render all page components and team select

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,36 +1,49 @@
 import React, {Component} from 'react';
 import Hero from './components/hero/Hero.jsx';
+import TeamSelect from './components/teamselect/Teamselect.jsx';
 
 /**
  * Main component for team page to render Hero, Info and Stats components.
- *
- * Props:
- *
  */
 class Team extends Component {
   constructor(props) {
     super(props);
 
+    // `index` -- whether to render index page with team selection (default landing page).
+    this.index = true;
+
     // State:
-    // `team` -- the team to display, updates styles and content based on selected team.
-    // `index` -- (bool) whether to render index page with team selection (default landing page).
     this.state = {
-      team: this.props.team,
-      index: true
+      // the team to display, updates styles and content based on selected team.
+      team: null
     };
+
+    this.updateTeam = this.updateTeam.bind(this);
+  }
+
+  // Change state of component to the chosen team
+  updateTeam(team) {
+    this.setState({team: team}, () => {
+      this.index = false;
+      console.log("SELECTED TEAM: " + team);
+    });
   }
 
   render() {
-    if (this.state.index) {
+    if (!this.index) {
       return(
-        <div className={"hero-container " + this.props.team}>
-          <Hero team={this.props.team} title={this.props.title} />
+        <div>
+          <div className={"hero-container " + this.state.team}>
+            <Hero team={this.state.team} title={this.state.title} />
+          </div>
+          <div className="info"></div>
+          <div className={"stats " + this.state.team}></div>
         </div>
       );
     }
     else {
       return(
-        <div><h2>SELECT TEAM</h2></div>
+        <TeamSelect handleOnClick={this.updateTeam} />
       );
     }
   }
