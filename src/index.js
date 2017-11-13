0b448d37f26331c7d2dba193ff93dee44062ca58
SmCTwelve <smctwelve@gmail.com>
2017-11-13 22:30:30 +0000

    Add todo.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import TeamInfo from './teams.json';
 import './styles/main.scss';
 
 if (module.hot) {
@@ -12,3 +11,18 @@ ReactDOM.render(
   <App />,
   document.getElementById("root")
 );
+
+/**
+ * TODO
+ *
+ * API:
+ *  Fetch team JSON data
+ *  Update team JSON data
+ *    Specify team parameter and the key to fetch, or array of keys
+ *  Use AJAX
+ *  Use composition - different objects/JSON files for entities e.g. driver to update a driver's stats and
+ *  automatically update elsewhere through reference to the driver object/file.
+ *
+ *  e.g.
+ *  updateTeam(ferrari, keys)
+ */
