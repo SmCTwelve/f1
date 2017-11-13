3ea515a0bed1cbbbb297b7f617711f725e3660fc
SmCTwelve <smctwelve@gmail.com>
2017-11-13 22:29:46 +0000

    Refactor to class component.

--- src/components/main/Team.jsx
+++ src/components/main/Team.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Hero from '../hero/Hero.jsx';
 import Nav from '../nav/Nav.jsx';
 
@@ -8,22 +8,43 @@ import Nav from '../nav/Nav.jsx';
  * Props: `team` -- the selected team.
  */
 
- const TeamView = (props) => (
-  <div>
-    <Nav team={props.team} index={false} />
-    <main id="main">
-      <div className={"hero-container " + props.team}>
-        <Hero team={props.team} />
+ class TeamView extends Component {
+   constructor(props) {
+     super(props);
+   }
+
+   componentDidMount() {
+     this.props.load(false);
+   }
+   componentDidUpdate() {
+     this.props.load(false);
+   }
+   componentWillUpdate() {
+     this.props.load(true);
+   }
+   componentWillMount() {
+     this.props.load(true);
+   }
+
+   render() {
+     return(
+      <div>
+        <Nav team={this.props.team} index={false} />
+        <main id="main">
+          <div className={"hero-container " + this.props.team}>
+            <Hero team={this.props.team} />
+          </div>
+          <div id="info" className="info">
+            <div className="content">
+            <h1 className="hdg">{this.props.team}</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro odio reiciendis illum corrupti, dolore esse, laborum, necessitatibus dolorem nostrum officiis quo delectus dolorum quibusdam? Quae maxime commodi expedita dignissimos. Quasi?</p>
+            </div>
+          </div>
+          <div className={"stats " + this.props.team}></div>
+        </main>
       </div>
-      <div id="info" className="info">
-        <div className="content">
-        <h1 className="hdg">{props.team}</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro odio reiciendis illum corrupti, dolore esse, laborum, necessitatibus dolorem nostrum officiis quo delectus dolorum quibusdam? Quae maxime commodi expedita dignissimos. Quasi?</p>
-        </div>
-      </div>
-      <div className={"stats " + props.team}></div>
-    </main>
-  </div>
- );
+     );
+   }
+ }
 
  export default TeamView;
