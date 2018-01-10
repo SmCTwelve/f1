9ce36ab6b1490f77b35cbcf9ccdd01b6262c2f4a
SmCTwelve <smctwelve@gmail.com>
2018-01-10 01:19:35 +0000

    Add classes and image placeholder

--- src/components/driver/Driver.jsx
+++ src/components/driver/Driver.jsx
@@ -9,20 +9,22 @@ import PoleWinChart from '../chart/PoleWinChart.jsx';
  * @param {*} props
  */
 const Driver = (props) => (
-  <div>
-    <div>
-      {/* image */}
+  <div className='driver-card'>
+    <div className='driver-info'>
+      <div className='driver-img'>
+        <div className="placeholder"></div>
+      </div>
+      <div>
+        <h3>{`${props.driver.firstName} ${props.driver.lastName}`}</h3>
+        <ul>
+          <li><strong>Code: </strong>{props.driver.code}</li>
+          <li><strong>Age: </strong>{props.driver.age}</li>
+          <li><strong>Nationality: </strong>{props.driver.nationality}</li>
+          <li><strong>Championships: </strong>{props.driver.stats.wdc}</li>
+        </ul>
+      </div>
     </div>
-    <div>
-      <h3>{`${props.driver.firstName} ${props.driver.lastName}`}</h3>
-      <ul>
-        <li><strong>Code: </strong>{props.driver.code}</li>
-        <li><strong>Age: </strong>{props.driver.age}</li>
-        <li><strong>Nationality: </strong>{props.driver.nationality}</li>
-        <li><strong>Championships: </strong>{props.driver.stats.wdc}</li>
-      </ul>
-    </div>
-    <div>
+    <div className='chart-pole-win'>
       <PoleWinChart driver={props.driver} />
     </div>
   </div>
