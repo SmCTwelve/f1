8f2353723ed484edc73c10c79421a5ba8e8e09c1
SmCTwelve <smctwelve@gmail.com>
2018-02-06 22:54:00 +0000

    Add padding to prevent scale cut off

--- src/components/chart/ComponentsChart.jsx
+++ src/components/chart/ComponentsChart.jsx
@@ -2,6 +2,17 @@ import React from 'react';
 import { Polar } from 'react-chartjs-2';
 import { driverComponents } from './charts.js';
 
+const options = {
+  layout: {
+    padding: {
+      top: 5,
+      bottom: 5
+    }
+  },
+  legend: {position: 'left'},
+  animation: {animateRotate: false},
+}
+
 /**
  * Render a polar chart showing engine components used for the driver.
  *
@@ -9,7 +20,7 @@ import { driverComponents } from './charts.js';
  */
 const ComponentsChart = (props) => {
   const chartData = driverComponents(props.driver);
-  return <Polar data={chartData}  />;
+  return <Polar data={chartData} options={options} />;
 }
 
 export default ComponentsChart;
