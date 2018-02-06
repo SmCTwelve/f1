b5a7c2fbcd61f0efee3341a6409f2601b86d889e
SmCTwelve <smctwelve@gmail.com>
2018-02-06 22:56:57 +0000

    Remove teams array, pass in as props instead

--- src/components/teamselect/Teamselect.jsx
+++ src/components/teamselect/Teamselect.jsx
@@ -1,13 +1,6 @@
 import React from 'react';
 import Logo from '../logo/Logo.jsx';
 
-// List of team names -- USE teams.json KEYS --
-const teams = [
-  'ferrari',
-  'redbull',
-  'renault'
-]
-
 /**
  * Default landing page to choose which team page to view. Component presents each team
  * logo to be clicked on, which will render the content for that team.
@@ -16,10 +9,12 @@ const teams = [
  * is set to 'true'. Clicking one of the logos sets the state to 'false' causing the page to re-render with the new
  * content. Pressing the Home link also renders the team selection.
  *
+ * @param props 'teams' -- Array of team names from App component
  */
 const TeamSelect = (props) => {
 
-  const logos = teams.map( (team, index) =>
+  // Map array of teams to array of Logo components to be rendered
+  const logos = props.teams.map( (team, index) =>
     <Logo key={index} team={team} {...props} />
   );
 
